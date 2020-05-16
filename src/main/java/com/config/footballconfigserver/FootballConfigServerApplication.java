3f6a53573af9768a2568ea44dd70ee87dfa85386
goalhits <goalhits@gmail.com>
2020-05-16 12:49:38 +0530

    Added football config directory

---
+++ src/main/java/com/config/footballconfigserver/FootballConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.config.footballconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballConfigServerApplication.class, args);
+	}
+
+}
